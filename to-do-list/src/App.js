dcd732f424e77a327c6d2ec13f410d0165936da9
Mahdi Kchaou <119625273+Mahdikchaw@users.noreply.github.com>
2024-06-20 14:10:33 +0200

    buidling the to do list algorithm

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -6,18 +6,32 @@ function App() {
   const inputRef = useRef();
 
   const AddTask = () => { 
+    console.log('#####',inputRef.current.value)
     const task = inputRef.current.value; 
-    setToDos([...toDos,task]); 
-  };
+    const newItem = { 
+      text : task, 
+      finished : false
+    } ; 
+    setToDos([...toDos,newItem]); 
+    inputRef.current.value = '';
+
+
+  }; 
+
+  const itemDone = (index) => { 
+    const newToDos = [...toDos];
+    newToDos[index].finished = !newToDos[index].finished; 
+    setToDos(newToDos);
+  } ;
 
   return (
     <div className="App">
-      {/* so we start an input an button to add your tasks */}
+      {/* so we start with an input and a button to add your tasks */}
       <input type='text' ref={inputRef} />
       <button onClick={AddTask}> Add task </button>
       <ul>
-        { toDos.map((task,index)=> (
-          <li key={index}>{task}</li>
+        { toDos.map(({text, finished},index)=> (
+          <li key={index} onClick={()=>itemDone(index)} className={finished ? "done" : ""}>{text}</li>
         ))}
 
       </ul>
