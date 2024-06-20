25ffb19fff131a9ebe668ade427d89d3e7c576a2
Mahdi Kchaou <119625273+Mahdikchaw@users.noreply.github.com>
2024-06-20 15:22:18 +0200

    some design features added

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 
 function App() {
   const [toDos, setToDos] = useState([]);
+  const [counter, setCounter] = useState(0);
   const inputRef = useRef();
-
   const AddTask = () => { 
-    console.log('#####',inputRef.current.value)
     const task = inputRef.current.value; 
     const newItem = { 
       text : task, 
@@ -14,28 +13,39 @@ function App() {
     } ; 
     setToDos([...toDos,newItem]); 
     inputRef.current.value = '';
-
-
   }; 
 
   const itemDone = (index) => { 
     const newToDos = [...toDos];
     newToDos[index].finished = !newToDos[index].finished; 
     setToDos(newToDos);
+    setCounter((prevCounter)=> prevCounter+1);
   } ;
 
   return (
     <div className="App">
       {/* so we start with an input and a button to add your tasks */}
-      <input type='text' ref={inputRef} />
-      <button onClick={AddTask}> Add task </button>
-      <ul>
-        { toDos.map(({text, finished},index)=> (
-          <li key={index} onClick={()=>itemDone(index)} className={finished ? "done" : ""}>{text}</li>
-        ))}
+      <div className='container'>
 
-      </ul>
+          <div className='headerOfTheList'>
+            <input className='enterText' placeholder='TO DO ...' type='text' ref={inputRef} />
+            <button className='button' onClick={AddTask}> Add Task </button>
+          </div>
 
+          <div className='listOfTasks'>
+            <ul>
+              { toDos.map(({text, finished},index)=> (
+                <li key={index} onClick={()=>itemDone(index)} className={finished ? "done" : "itemNotDone"}>{text}</li>
+              ))}
+
+            </ul>
+          </div>
+          <div className='listFooter'>
+            <p>Completed {counter} of {toDos.length}</p>
+          </div>
+
+      </div>
+     
 
     </div>
   );
--- to-do-list/src/App.test.js
+++ to-do-list/src/App.test.js
@@ -1,8 +1,63 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App.test';
+import { useState, useRef } from 'react';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+});export function App() {
+  const [toDos, setToDos] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const inputRef = useRef();
+
+  const AddTask = () => {
+    console.log('#####', inputRef.current.value);
+    const task = inputRef.current.value;
+    const newItem = {
+      text: task,
+      finished: false
+    };
+    setToDos([...toDos, newItem]);
+    inputRef.current.value = '';
+
+
+  };
+
+  const itemDone = (index) => {
+    const newToDos = [...toDos];
+    newToDos[index].finished = !newToDos[index].finished;
+    setToDos(newToDos);
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  return (
+    <div className="App">
+      {/* so we start with an input and a button to add your tasks */}
+      <div className='container'>
+
+        <div className='headerOfTheList'>
+          <input className='enterText' placeholder='TO DO ...' type='text' ref={inputRef} />
+          <button className='button' onClick={AddTask}> Add Task </button>
+        </div>
+
+        <div className='listOfTasks'>
+          <ul>
+            {toDos.map(({ text, finished }, index) => (
+              <li key={index} onClick={() => itemDone(index)} className={finished ? "done" : "itemNotDone"}>{text}</li>
+            ))}
+
+          </ul>
+        </div>
+        <div className='listFooter'>
+          <img src="https://i.pinimg.com/564x/3c/9e/4b/3c9e4b32b67d641965915ee08db63844.jpg" className='image' />
+          <p>Completed {counter} of {toDos.length}</p>
+        </div>
+
+      </div>
+
+
+    </div>
+  );
+}
+
