4f66f2ffbad05efddf03bddef268b9489bac529b
Mahdi Kchaou <119625273+Mahdikchaw@users.noreply.github.com>
2024-06-26 16:17:13 +0200

    finalizing the project

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -5,6 +5,9 @@ function App() {
   const [toDos, setToDos] = useState([]);
   const [counter, setCounter] = useState(0);
   const inputRef = useRef();
+  
+
+
   const AddTask = () => { 
     const task = inputRef.current.value; 
     const newItem = { 
@@ -15,12 +18,35 @@ function App() {
     inputRef.current.value = '';
   }; 
 
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      AddTask();
+    }
+  };
+  const transformDescription = (event,index) =>{
+    if (event.key === "Enter"){ 
+      const newToDos = [...toDos];
+      newToDos[index].description = event.target.value;
+      setToDos(newToDos);
+    }
+  };
+
   const itemDone = (index) => { 
     const newToDos = [...toDos];
     newToDos[index].finished = !newToDos[index].finished; 
     setToDos(newToDos);
-    setCounter((prevCounter)=> prevCounter+1);
+    if (newToDos[index].finished === true){
+      setCounter((prevCounter)=> prevCounter+1);
+    } 
+    else { 
+      setCounter((prevCounter)=> prevCounter-1);
+    }
+    
   } ;
+
+  const returnFunction = (index) =>{ 
+    return itemDone(index);
+  }
 
   return (
     <div className="App">
@@ -28,14 +54,32 @@ function App() {
       <div className='container'>
 
           <div className='headerOfTheList'>
-            <input className='enterText' placeholder='TO DO ...' type='text' ref={inputRef} />
-            <button className='button' onClick={AddTask}> Add Task </button>
+            <input className='enterText' placeholder='To do ...' type='text' ref={inputRef} onKeyDown={handleKeyPress} />
+            {/* <button className='button' onClick={AddTask}> Add Task </button> */}
           </div>
 
           <div className='listOfTasks'>
             <ul>
-              { toDos.map(({text, finished},index)=> (
-                <li key={index} onClick={()=>itemDone(index)} className={finished ? "done" : "itemNotDone"}>{text}</li>
+              { toDos.map(({text, finished,description},index)=> (
+                <div key={index}>
+                    <div className='task'>
+                      <div className='circleContainer'>
+                        <div className='circle' onClick={()=>returnFunction(index)}></div>
+                      </div>
+                      <li key={index} onClick={()=>itemDone(index)} className={finished ? "done" : "itemNotDone"}>{text}</li>                       
+                    </div>              
+                    
+                      {description ? (
+                          <p>{description}</p> ) : (               
+                          <input 
+                            className='description' 
+                            placeholder='Add your description' 
+                            onKeyDown={(e) => transformDescription(e, index)} 
+                          />
+                       )}
+                     {/* <input className='description' placeholder='add you description' onKeyDown={transformDescription} /> */}
+                </div>
+             
               ))}
 
             </ul>
